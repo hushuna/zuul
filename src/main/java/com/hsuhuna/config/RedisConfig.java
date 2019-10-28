b7ef8025dfa1e930c1654f4a0c02572f725e7f55
hushuna <19945096907@136.com>
2019-10-28 10:33:16 +0800

    网关

--- src/main/java/com/hsuhuna/config/RedisConfig.java
+++ src/main/java/com/hsuhuna/config/RedisConfig.java
@@ -13,20 +13,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-/*    @Bean(name="genericJackson2JsonRedisSerializer")
-    public GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
-        return new GenericJackson2JsonRedisSerializer();
-    }
-
-    @Bean(name = "myRedisTemplate")
-    public RedisTemplate<Object,Object> myRedisTemplate(RedisConnectionFactory redisConnectionFactory, GenericJackson2JsonRedisSerializer ser){
-        RedisTemplate template = new RedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-        //设置序列化方法
-        template.setDefaultSerializer(ser);
-        return template;
-    }*/
-
     /**
      * 配置redistemplate
      *
