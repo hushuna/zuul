4750016d20b0c12a7327e1c56d820b403d3e5a1a
hushuna <19945096907@136.com>
2019-10-28 10:05:56 +0800

    网关

---
+++ src/main/java/com/hsuhuna/filter/Filter.java
@@ -0,0 +1,61 @@
+package com.hsuhuna.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpRequest;
+import org.apache.http.protocol.RequestContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Filter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(Filter.class);
+
+
+
+    /**
+     * 返回一个字符串，代表过滤器的类型
+     * pre表示被路由之前
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run(){
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(request.getRequestURI().toString());
+
+        String token = request.getParameter("token");// 获取请求的参数
+
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+    }
+}
